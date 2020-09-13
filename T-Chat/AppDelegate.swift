47e791d9f3e73025ec215cf0a3ed2d0ed42b901b
Dmitrii <dp20121989@mail.ru>
2020-09-13 23:59:53 +0300

    Removed sceneDelegate.swift; Added window var to the appDelegate.swift; Removed scene configuration from info.plist. App lifecycle messages added to the app.

---
+++ T-Chat/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  T-Chat
+//
+//  Created by Дмитрий on 13.09.2020.
+//  Copyright © 2020 DP. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print("Application moved from <Not running> to <Incactive>: <\(#function)>")
+        return true
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("Application moved from <Background> to <Inactive>: <\(#function)>")
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("Application moved from <Inactive> to <Active>: <\(#function)>")
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("Application moved from <Active> to <Inactive>: <\(#function)>")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Application moved from <Inactive> to <Background>: <\(#function)>")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("State changed to \(#function)")
+    }
+    
+    
+
+
+
+}
+
---
+++ T-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T-Chat
+//
+//  Created by Дмитрий on 13.09.2020.
+//  Copyright © 2020 DP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
