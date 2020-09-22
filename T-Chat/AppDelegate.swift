d185cf48590a6bd69d95ae30146b95d05d67de5f
Dmitrii <dp20121989@mail.ru>
2020-09-22 14:04:52 +0300

    add application(_ application: willFinishLaunchingWithOptions:) func to AppDelegate

--- T-Chat/AppDelegate.swift
+++ T-Chat/AppDelegate.swift
@@ -25,6 +25,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
         logMessage(message: "Application moved from <Not running> to <Incactive>: <\(#function)>")
         return true
     }
