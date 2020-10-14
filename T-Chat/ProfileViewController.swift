b9775a15b9c8c40152354777f210094cd65b144d
Dmitrii <dp20121989@mail.ru>
2020-10-15 01:48:55 +0300

    very poor realization of the GCD saving and getting data

--- T-Chat/ProfileViewController.swift
+++ T-Chat/ProfileViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 import AVFoundation
 
 class ProfileViewController: UIViewController {
+    
+    lazy var activityIndicator = UIActivityIndicatorView()
+    var currentFullName: String = ""
+    var currentAboutYouself: String = ""
 
+    @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var gcdSaveButton: UIButton!
     @IBOutlet weak var operationalSaveButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
@@ -22,16 +27,30 @@ class ProfileViewController: UIViewController {
 
     @IBAction func editProfileButtonTapped(_ sender: UIButton) {
         if sender.titleLabel?.text == "Edit" {
+            currentFullName = nameAndSurnameTextField.text ?? ""
+            currentAboutYouself = aboutYouselfTextView.text
             sender.setTitle("Cancel", for: .normal)
             editProfileOn()
         } else {
             sender.setTitle("Edit", for: .normal)
             editProfileOff()
+            nameAndSurnameTextField.text = currentFullName
+            aboutYouselfTextView.text = currentAboutYouself
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getProfileInfo()
+        
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
         
         nameAndSurnameTextField.delegate = self
         aboutYouselfTextView.delegate = self
@@ -40,7 +59,7 @@ class ProfileViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
 
         profileImageView.isHidden = true
-        avatarEditButton.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+        avatarEditButton.addTarget(self, action: #selector(avatarEditButtonTapped), for: .touchUpInside)
         nameLabel.text = getNameLabelLetters()
         aboutYouselfTextView.layer.cornerRadius = 5
         nameAndSurnameTextField.layer.cornerRadius = 5
@@ -57,7 +76,7 @@ class ProfileViewController: UIViewController {
         operationalSaveButton.layer.cornerRadius = gcdSaveButton.bounds.height / 4
     }
     
-    @objc func editButtonTapped() {
+    @objc func avatarEditButtonTapped() {
         let ac = UIAlertController(title: "Edit profile photo", message: nil, preferredStyle: .actionSheet)
         
         // action для выбора фотографии из галереи
@@ -89,6 +108,7 @@ class ProfileViewController: UIViewController {
         }
         return finalStr.uppercased()
     }
+    
 //    Метод для инициализации viewController с помощью имени сториборда. Имена контроллера и сториборда одинаковые для того, чтобы не использовать storyboardID для инициализации.
     static func storyboardInstance() -> ProfileViewController? {
         let storyboard = UIStoryboard(name: String(describing: self), bundle: nil)
@@ -96,12 +116,20 @@ class ProfileViewController: UIViewController {
     }
     
     @objc func gcdButtonTapped() {
-        navigationItem.rightBarButtonItem?.title = "Edit"
         editProfileOff()
+        guard let fullName = nameAndSurnameTextField.text else { return }
+        if fullName != currentFullName {
+            saveStringData(nameAndSurnameTextField.text ?? "", withFileName: "fullName")
+        }
+        if aboutYouselfTextView.text != currentAboutYouself {
+            saveStringData(aboutYouselfTextView.text, withFileName: "aboutYouSelf")
+        }
+        if let profilePhoto = profileImageView.image {
+            saveImageData(profilePhoto)
+        }
     }
     
     @objc func operationalButtonTapped() {
-        navigationItem.rightBarButtonItem?.title = "Edit"
         editProfileOff()
     }
     
@@ -112,6 +140,112 @@ class ProfileViewController: UIViewController {
     
     @objc func keyboardWillHide(notification: NSNotification) {
       self.view.frame.origin.y = 0
+    }
+    
+    func getProfileInfo() {
+        let documentsDirectory = getDocumentsDirectory()
+        let fullNamePath = documentsDirectory.appendingPathComponent("fullName").appendingPathExtension("txt")
+        let aboutYouselfPath = documentsDirectory.appendingPathComponent("aboutYouSelf").appendingPathExtension("txt")
+        let profilePhotoPath = documentsDirectory.appendingPathComponent("profilePhoto").appendingPathExtension("png")
+        activityIndicator.startAnimating()
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            do {
+                let fullNameData = try Data(contentsOf: fullNamePath)
+                if let fullName = String(data: fullNameData, encoding: .utf16) {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.nameAndSurnameTextField.text = fullName
+                        self?.nameLabel.text = self?.getNameLabelLetters()
+                    }
+                }
+                let aboutYouselfData = try Data(contentsOf: aboutYouselfPath)
+                if let aboutYouself = String(data: aboutYouselfData, encoding: .utf16) {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.aboutYouselfTextView.text = aboutYouself
+                    }
+                }
+                let profileImageData = try Data(contentsOf: profilePhotoPath)
+                if let profilePhoto = UIImage(data: profileImageData) {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.profileImageView.image = profilePhoto
+                        self?.profileImageView.isHidden = false
+                        self?.nameLabel.isHidden = true  
+                    }
+                }
+                DispatchQueue.main.async { [weak self] in
+                    self?.activityIndicator.stopAnimating()
+                }
+            } catch {
+                print("can't read data")
+                DispatchQueue.main.async { [weak self] in
+                    self?.activityIndicator.stopAnimating()
+                }
+            }
+        }
+    }
+    
+    func saveStringData(_ text: String, withFileName name: String) {
+        activityIndicator.startAnimating()
+        let ac = getDefaultAlertController(withTitle: "Data was saved", andMessage: nil)
+        let queue = DispatchQueue.global(qos: .default)
+        let path = getDocumentsDirectory().appendingPathComponent(name).appendingPathExtension("txt")
+        if let data = text.data(using: .utf16) {
+            queue.async {
+                do {
+                    try data.write(to: path)
+                    DispatchQueue.main.async { [weak self] in
+                        self?.activityIndicator.stopAnimating()
+                        self?.present(ac, animated: true)
+                    }
+                                    } catch {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.activityIndicator.stopAnimating()
+                        ac.title = "Error"
+                        ac.message = "Couldn't save data"
+                        ac.addAction(UIAlertAction(title: "Repeat", style: .default, handler: { [weak self] _ in
+                            self?.saveStringData(text, withFileName: name)
+                        }))
+                    }
+                }
+            }
+        }
+    }
+    
+    func saveImageData(_ image: UIImage) {
+        activityIndicator.startAnimating()
+        let ac = getDefaultAlertController(withTitle: "Data was saved", andMessage: nil)
+        let queue = DispatchQueue.global(qos: .default)
+        let path = getDocumentsDirectory().appendingPathComponent("profilePhoto").appendingPathExtension("png")
+        if let data = image.pngData() {
+            queue.async {
+                do {
+                    try data.write(to: path)
+                    DispatchQueue.main.async { [weak self] in
+                        self?.activityIndicator.stopAnimating()
+                        self?.present(ac, animated: true)
+                    }
+                } catch {
+                    DispatchQueue.main.async { [weak self] in
+                        self?.activityIndicator.stopAnimating()
+                        ac.title = "Error"
+                        ac.message = "Couldn't save data"
+                        ac.addAction(UIAlertAction(title: "Repeat", style: .default, handler: { [weak self] _ in
+                            self?.saveImageData(image)
+                        }))
+                    }
+                }
+            }
+        }
+    }
+    
+    func getDefaultAlertController(withTitle title: String, andMessage message: String?) -> UIAlertController {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        return ac
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     private func editProfileOn() {
@@ -128,6 +262,7 @@ class ProfileViewController: UIViewController {
         aboutYouselfTextView.isEditable = false
         aboutYouselfTextView.backgroundColor = .white
         buttons(disable: [gcdSaveButton, operationalSaveButton, avatarEditButton])
+        editButton.setTitle("Edit", for: .normal)
         resignFirstResponder()
     }
     
